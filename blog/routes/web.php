a5ce19c318c1544ef270c73aca9207f2eaee30d3
junhao <17180846789@163.com>
2020-06-25 20:41:57 +0800

    is_one

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
+});
+
+
+Route::get('/home', function () {
+  return 'laravel.jh';
 });
